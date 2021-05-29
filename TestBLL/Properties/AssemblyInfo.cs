b1e9a928bdb9cdc4e8583ae318cbc5c7423df87c
ancoutard <antcout63@gmail.com>
2021-05-29 09:53:56 +0200

    ajout projet de test

---
+++ TestBLL/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestBLL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestBLL")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92981dfa-3bfe-4fc6-bcf5-a7eac016e7fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestBLL/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogicLayer;
+using ClassLibrary1.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace TestBLL
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetAllEmploye_RetoureListEmploye()
+        {
+            Manager manager = Manager.Instance;
+            Assert.IsInstanceOfType(manager.GetAllEmploye(), typeof(List<Employe>));
+        }
+    }
+}
